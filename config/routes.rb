9cf089b760064f7a4a79453485a938d6fc5305f4
Ramadhan Aheebwa <raaheebwa@gmail.com>
2020-04-13 16:48:11 +0300

    feat: landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/about', to: 'home#about'
+  get '/contact', to: 'home#contact'
+
+  root to: 'home#index', as: :homepage
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
