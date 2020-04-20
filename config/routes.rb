eff298a2a4c001ac9e0c1ee53693175f0efe0930
Ramadhan Aheebwa <raaheebwa@gmail.com>
2020-04-20 04:20:37 +0300

    UI: frontend with Zaka Calc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
+  get '/calculator', to: 'home#calculator'
 
   root to: 'home#index', as: :homepage
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
