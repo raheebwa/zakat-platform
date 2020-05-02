02ae1a555b301b0bd47527906e9857064e1c516c
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-05-02 22:02:04 +0300

    feat: can sign in with all details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
   get '/calculator', to: 'home#calculator'
