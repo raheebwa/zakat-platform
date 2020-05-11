66f78b4ec8c1697bef3ec6876e3064a623f9df79
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-05-11 10:09:55 +0300

    feat: addeed admin dash

--- Gemfile
+++ Gemfile
@@ -47,3 +47,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Auth
 gem 'devise'
+
+#admin Dash
+gem "administrate"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :payments
+      resources :mosques
+
+      root to: "users#index"
+    end
   resources :payments
   devise_for :users, controllers: {
     registrations: 'users/registrations'
@@ -10,6 +17,6 @@ Rails.application.routes.draw do
   post '/pay-zakat', to: 'home#pay_zakat'
   delete "/my-payments/:id", to: "home#destroy_payment", as: :zakat_payment_path
 
-  root to: 'home#index', as: :homepage
+  root to: 'home#index', as: :root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
